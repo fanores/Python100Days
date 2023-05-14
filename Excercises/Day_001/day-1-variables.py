8f65fd74d430d21769e55421f44d3def4d6e55d0
Stevo Misik <fanores@gmail.com>
2023-05-14 16:06:30 +0200

    Day 1 - variables

---
+++ Excercises/Day_001/day-1-variables.py
@@ -0,0 +1,25 @@
+# Variables
+yourName = input("What is your name? ")
+print("Your name is " + yourName + ".")
+
+nameLenth = str(len(yourName))
+print("The lenght of your name is " + nameLenth + ".")
+
+# Variable excercise
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+temp_a = a
+a = b
+b = temp_a
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
