6ac5befb68e6e3eca6f1623d3003fe14d07237cd
Stefan Misik <stefan.misik@Stefans-MacBook-Pro.local>
2023-05-14 13:46:06 +0200

    Day 1 - printing

---
+++ Excercises/Day_001/day-1-printing.py
@@ -0,0 +1,13 @@
+# Write your code below this line 👇
+# Write multiple lines with single PRINT command
+print("Hello world!\nHello world!\n3rd Line\n")
+
+# String concatenation
+print("Hello" + " " + "Stevo" + "\n")
+
+# Input function
+print("Input function test.")
+# input("What is your name:")
+
+print("Hello, " + input("What is your name?") + "!\n")
+print(len(input("What is your name? \n")))
