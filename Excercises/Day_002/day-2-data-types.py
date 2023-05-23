6031694adb229f424bace8913dd5cf113cb58c33
Stevo Misik <fanores@gmail.com>
2023-05-23 21:26:17 +0200

    Day 2 - data types

---
+++ Excercises/Day_002/day-2-data-types.py
@@ -0,0 +1,14 @@
+####################################
+# Exercise data types
+####################################
+
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+number_1 = int(two_digit_number[0])
+number_2 = int(two_digit_number[1])
+sum = number_1 + number_2
+print(sum)
