f26858e273fb5ee0b8baa67638d96dcbefac87b1
Stevo Misik <fanores@gmail.com>
2024-03-24 21:27:15 +0100

    Day 3 - odd or even and modulo

---
+++ Excercises/Day_003/day-3-odd-even-modulo.py
@@ -0,0 +1,13 @@
+####################################
+# Odd or Even (modulo)
+####################################
+
+# Which number do you want to check?
+number = int(input("Enter your number: "))
+# 🚨 Don't change the code above 👆
+
+# Write your code below this line 👇
+if (number % 2) == 0:
+    print("This is an even number.")
+else:
+    print("This is an odd number.")
