d82ed7d2ffe3abc297e2831ab6d1d0e176bcbbfe
Stevo Misik <fanores@gmail.com>
2024-03-24 21:20:22 +0100

    Day 3 - control flow

---
+++ Excercises/Day_003/day-3-control-flow.py
@@ -0,0 +1,25 @@
+####################################
+# Control Flow
+####################################
+
+# Example of IF / ELSE condition in Python:
+# if condition:
+#   do this
+# else:
+#   do that
+
+# Functioning example:
+water_level = 50
+if water_level > 80:
+    print("Drain water.")
+else:
+    print("Continue.")
+
+# Another conditional
+print("Welcome to my fair.")
+your_height = int(input("Enter your height in cm. "))
+
+if your_height >= 120:
+    print("You can enter my fair.")
+else:
+    print("You cannot enter my fair, grow taller.")
