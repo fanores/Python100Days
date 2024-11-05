9b8c88ac9654428357dc409915ef638abbebb60c
Stefan Misik <stefan.misik@sap.com>
2024-11-05 08:34:25 +0100

    Day 4 - Who Pays The Bill

---
+++ Excercises/Day_004/day_4_pay_bill.py
@@ -0,0 +1,14 @@
+####################################
+# Python Pay The Bill Exercise
+####################################
+import random
+
+friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+
+# Option #1
+print("Option 1:")
+print(random.choice(friends))
+
+# Option #2
+random_index = random.randint(0, 4)
+print(friends[random_index])
