02741a136a8224e7f3373eea5ab1f9ae96046350
Stefan Misik <stefan.misik@sap.com>
2024-11-11 08:26:46 +0100

    Day 5 - Python Loops

---
+++ Excercises/Day_005/day_5_loops.py
@@ -0,0 +1,12 @@
+#######################################
+# Python Loops
+#######################################
+
+# FOR LOOP
+# for item in list_of_items:
+#   do something to each item
+
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+    print(fruit)
+    print(fruit + " pie")
