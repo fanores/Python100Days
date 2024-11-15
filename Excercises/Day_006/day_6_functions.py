4af1a07738153cf9896000d9f5adde21a1d13e3f
Stefan Misik <stefan.misik@sap.com>
2024-11-15 08:12:18 +0100

    Day 6 - Functions

---
+++ Excercises/Day_006/day_6_functions.py
@@ -0,0 +1,18 @@
+##########################################
+# Python Functions - Define a Function
+##########################################
+
+# Built-in Python Functions
+# https://docs.python.org/3/library/functions.html
+
+# Inspiration is the Reeborg's World -> something like Robot Karel
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json
+
+# Define own function
+def say_hello_goodbye():
+    print("Hello")
+    print("Bye")
+
+
+# Run the function
+say_hello_goodbye()
