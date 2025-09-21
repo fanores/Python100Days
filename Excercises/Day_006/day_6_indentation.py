eef3e7912c17ed0bd66ef008994bf772dd8f77d3
Stefan Misik <stefan.misik@sap.com>
2025-09-21 06:37:48 +0200

    Day 6 - Indentation

---
+++ Excercises/Day_006/day_6_indentation.py
@@ -0,0 +1,15 @@
+##########################################
+# Python Functions - Indentation
+##########################################
+# Each indentation is represented by 4 spaces
+
+# Example of indentation in Python
+def my_function():
+    print("This line is indented with 4 spaces.")
+    if True:
+        print("This line is indented with 8 spaces (4 + 4).")
+    else:
+        print("This line is also indented with 8 spaces (4 + 4).")
+    print("Back to 4 spaces indentation.")
+
+print("This line is not indented.")
