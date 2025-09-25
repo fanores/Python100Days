7295dbd77657a453005ca21ae9602d0d5141904c
Stefan Misik <stefan.misik@sap.com>
2025-09-25 19:25:33 +0200

    Day 6 - While Loop

---
+++ Excercises/Day_006/day_6_while_loop.py
@@ -0,0 +1,12 @@
+##########################################
+# Python Functions - While Loop
+##########################################
+
+# Example of a while loop in Python
+# while something_is_true:
+#     do something
+
+number = int(input("Input a number, ranging 1...10: "))
+while number > 0:
+    print(f"The number is now {number}, lets decrease it.")
+    number -= 1
