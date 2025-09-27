549c36d734ae0cee1baa7e0fb524be6048bc258b
Stefan Misik <stefan.misik@sap.com>
2025-09-27 21:03:12 +0200

    Day 7 - Description of Hangman Task

---
+++ Excercises/Day_007/day_7_hangman.py
@@ -0,0 +1,16 @@
+##########################################
+# Python Project - Hangman Game
+##########################################
+
+# What is a Hangman Game?
+# Go to Wikipedia: https://en.wikipedia.org/wiki/Hangman_(game)
+# Or give it a try here: https://hangmanwordgame.com/
+
+# Step 1: Choose a word from a list of words
+# Step 2: Ask the user to guess a letter
+# Step 3: Check if the letter is in the word
+# Step 4: If the letter is in the word, reveal its position(s)
+# Step 5: If the letter is not in the word, decrease the number of attempts
+# Step 6: Repeat steps 2-5 until the user guesses the word or runs out of attempts
+# Step 7: Congratulate the user if they win, or reveal the word if they lose
+
