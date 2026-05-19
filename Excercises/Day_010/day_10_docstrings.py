67168ecd6af484ee46399ad9afa5c1492e04a467
Stefan Misik <stefan.misik@sap.com>
2026-05-19 21:57:02 +0200

    Day 10 - Functions - Docstrings

---
+++ Excercises/Day_010/day_10_docstrings.py
@@ -0,0 +1,11 @@
+##################################################
+# Python - Docstrings
+##################################################
+
+# Example of a function with a docstring
+def my_function_with_docstring():
+    """This is a docstring. It explains what the function does."""
+    pass
+
+# Call the function and hoover over it to see the docstring
+my_function_with_docstring()
