4a00a6a0013bc9349474dab3f492be5eaffef7a4
Stefan Misik <stefan.misik@sap.com>
2026-05-27 09:56:37 +0200

    Day 12 - Block Scope

--- Excercises/Day_012/day_12_local_global_scope.py
+++ Excercises/Day_012/day_12_local_global_scope.py
@@ -17,3 +17,12 @@ def my_function():
 my_function()
 print(f"Outside the function, my_variable is: {my_variable}")
 print(f"Outside the function, my_other_global_variable is: {my_other_global_variable}")
+
+# There is no block scope in Python, only function scope. This means that variables defined inside a function are not accessible outside of that function, but variables defined outside of a function (global variables) are accessible inside the function unless they are shadowed by a local variable with the same name.
+game_level = 1
+
+if game_level < 5:
+    level_up_message = "You have leveled up!"
+    
+# Print content of level_up_message variable outside of the IF BLOCK to demonstrate that it is accessible due to the lack of block scope in Python.
+print(level_up_message)
